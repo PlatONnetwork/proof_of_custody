d90b6c5949e395fa0ce189414fb7f2bd73c45ce6
ujnss <unlinunlin@163.com>
2020-05-06 15:27:42 +0800

    clean

--- ud_socket.h
+++ ud_socket.h
@@ -1,158 +1,119 @@
+#pragma once
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <iostream>
 #include <string>
 using namespace std;
 
 class UnixSOcket {
  public:
   int partyid_ = -1;
-  string sname;
+  string socket_path_ = "";
 
   UnixSOcket(int partyid) : partyid_(partyid) {
-    sname = string("server_socket" + to_string(partyid));
+    socket_path_ = string("/tmp/server_socket" + to_string(partyid));
   }
 };
 
 class UnixClient : public UnixSOcket {
- public:
   using UnixSOcket::UnixSOcket;
+  int client_sockfd_;
 
-  int server_sockfd, client_sockfd;
-  int sockfd;
-
+ public:
   int init() {
     int len;
     struct sockaddr_un address;
     int result;
-    int i, byte;
-    char send_buf[128], ch_recv;
 
-    if ((sockfd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-      perror("socket");
+    if ((client_sockfd_ = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+      cerr << "socket error!" << endl;
       exit(EXIT_FAILURE);
     }
 
     address.sun_family = AF_UNIX;
-    strcpy(address.sun_path, sname.c_str());
+    strcpy(address.sun_path, socket_path_.c_str());
     len = sizeof(address);
 
+    int time_retry = 20000; //us
+    int retries = 60 * 1000 * 1000 / time_retry; // 1 min
+    bool connect_ok = false;
     do {
-      result = connect(sockfd, (struct sockaddr*)&address, len);
-      if (result == -1) {
-        //cout << "00000000000000000000000000000000000000000000" << endl;
-        usleep(20000);
-        continue;
-        printf("ensure the server is up\n");
-        perror("connect");
-        exit(EXIT_FAILURE);
+      result = connect(client_sockfd_, (struct sockaddr*)&address, len);
+      if (result == 0) {
+        connect_ok = true;
+        break;
       }
-      break;
-    } while (true);
-
+      usleep(time_retry);
+    } while (retries-- > 0);
+    if (!connect_ok) {
+      cerr << "ensure the server is up!" << endl;
+      exit(EXIT_FAILURE);
+    }
     return 0;
   }
 
   int send(const char* data, int len) {
-    int i, byte;
-    if ((byte = write(sockfd, data, len)) == -1) {
-      perror("write");
+    int byte;
+    if ((byte = write(client_sockfd_, data, len)) == -1) {
+      cerr << "write error!" << endl;
       exit(EXIT_FAILURE);
     }
 
     return 0;
   }
-  int send_to_server() {
-    int i, byte;
-    char send_buf[128], ch_recv;
-    for (i = 0; i < 5; i++) {
-      sprintf(send_buf, "client massage %d", i); 
-      if ((byte = write(sockfd, send_buf, sizeof(send_buf))) == -1) {
-        perror("write");
-        exit(EXIT_FAILURE);
-      }
-      if ((byte = read(sockfd, &ch_recv, 1)) == -1) {
-        perror("read");
-        exit(EXIT_FAILURE);
-      }
-      printf("receive from server data is: %c\n", ch_recv);
-    }
-    return 0;
-  }
   int uninit() {
-    close(sockfd);
+    close(client_sockfd_);
     return 0;
   }
 };
 
 class UnixServer : public UnixSOcket {
- public:
   using UnixSOcket::UnixSOcket;
+  int server_sockfd_, client_sockfd_;
 
-  int server_sockfd, client_sockfd;
-
+ public:
   int init() {
     int server_len, client_len;
     struct sockaddr_un server_address;
     struct sockaddr_un client_address;
-    int i, byte;
-    char ch_send, recv_buf[128];
 
-    unlink(sname.c_str());
-    server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0); 
-	  
+    unlink(socket_path_.c_str());
+    server_sockfd_ = socket(AF_UNIX, SOCK_STREAM, 0);
+
     server_address.sun_family = AF_UNIX;
-    strcpy(server_address.sun_path, sname.c_str());
+    strcpy(server_address.sun_path, socket_path_.c_str());
     server_len = sizeof(server_address);
 
-    bind(server_sockfd, (struct sockaddr*)&server_address, server_len);
+    bind(server_sockfd_, (struct sockaddr*)&server_address, server_len);
 
-    listen(server_sockfd, 5);
+    listen(server_sockfd_, 5);
 
-    printf("server waiting for  client connect\n");
     client_len = sizeof(client_address);
-	
-    client_sockfd =
-      accept(server_sockfd, (struct sockaddr*)&client_address, (socklen_t*)&client_len);
-    printf("the server wait form client data\n");
+    client_sockfd_ =
+      accept(server_sockfd_, (struct sockaddr*)&client_address, (socklen_t*)&client_len);
 
     return 0;
   }
 
   int recv(char* data, int len) {
-    int i, byte;
+    int byte;
 
-    if ((byte = read(client_sockfd, data, len)) == -1) {
-      perror("read");
+    if ((byte = read(client_sockfd_, data, len)) == -1) {
+      cerr << "read error!" << endl;
       exit(EXIT_FAILURE);
     }
 
     return 0;
   }
-  int recv_from_client() {
-    int i, byte;
-    char ch_send, recv_buf[128];
-    for (i = 0, ch_send = '1'; i < 5; i++, ch_send++) {
-      if ((byte = read(client_sockfd, recv_buf, sizeof(recv_buf))) == -1) {
-        perror("read");
-        exit(EXIT_FAILURE);
-      }
-      printf("the massage receiver from client is: %s\n", recv_buf);
-      sleep(1);
-      if ((byte = write(client_sockfd, &ch_send, 1)) == -1) {
-        perror("write");
-        exit(EXIT_FAILURE);
-      }
-    }
-    return 0;
-  }
   int uninit() {
-    //close(client_sockfd);
-    close(server_sockfd);
-    unlink(sname.c_str());
+    //close(client_sockfd_);
+    close(server_sockfd_);
+    unlink(socket_path_.c_str());
     return 0;
   }
 };
