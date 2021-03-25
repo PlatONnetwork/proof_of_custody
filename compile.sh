fdd1a14a32889d52e787e71a0432a69861c37cb9
ujnss <unlinunlin@163.com>
2021-03-25 10:22:28 +0800

    update compile script

--- compile.sh
+++ compile.sh
@@ -11,25 +11,27 @@ cd ${builddir}
 cmake .. -DCMAKE_INSTALL_PREFIX=${install_dir} -DCMAKE_PREFIX_PATH=${install_dir}
 make -j8
 
-# run examples
+# run examples and tests
 export LD_LIBRARY_PATH=${install_dir}/lib:$LD_LIBRARY_PATH
 
-cd ${curdir}
-# ./compile.py tutorial
-# Scripts/mascot.sh tutorial
-# cp ${builddir}/mascot-party.x ./ -f
-# ./mascot-party.x 1 tutorial -pn 27021 -h localhost -N 2 >logs/tutorial-1 2>&1 &
-# ./mascot-party.x 0 tutorial -pn 27021 -h localhost -N 2 | tee logs/tutorial-0 2>&1
+parties=3
+# run_tests <program name>
+function run_tests() {
+  cd ${curdir}
+  prog=$1
+  if [ ! -f "${builddir}/${prog}" ]; then
+    return
+  fi
 
-# #2
-Scripts/setup-ssl.sh 2
-cp ${builddir}/mascot-example.x ./ -f
-./mascot-example.x 1 2 >logs/mascot-example2-1 2>&1 &
-./mascot-example.x 0 2 | tee logs/mascot-example2-0 2>&1
-
-# #3
-# sleep 3
-# Scripts/setup-ssl.sh 3
-# ./mascot-example.x 2 3 >logs/mascot-example3-2 2>&1 &
-# ./mascot-example.x 1 3 >logs/mascot-example3-1 2>&1 &
-# ./mascot-example.x 0 3 | tee logs/mascot-example3-0 2>&1
+  echo -e "run ${prog} ...\n"
+  sleep 1
+  Scripts/setup-ssl.sh $parties
+  cp -f ${builddir}/${prog} ./
+  for ((i = 1; i < $parties; i++)); do
+    ./${prog} $i $parties >logs/${prog}-$i 2>&1 &
+  done
+  ./${prog} 0 $parties | tee logs/${prog}-0 2>&1
+}
+run_tests mascot_example.x
+run_tests poc_online_op_test.x
+run_tests poc_main.x
